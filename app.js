ad7442a023590e9f5c33dc93e2115db2aeda8f37
Ary Davani <arydavani@gmail.com>
2017-06-19 16:57:51 -0400

    upgraded to hard mode

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const loremIpsum = require('lorem-ipsum');
-var output = loremIpsum({
-    count: 3,
+
+function generateHTML(num) {
+  var count = num ? num : 3;
+  return loremIpsum({
+    count: count,
     units: 'paragraphs',
     sentenceLowerBound: 10,
     sentenceUpperBound: 18,
     paragraphLowerBound: 5,
     paragraphUpperBound: 12,
     format: 'html',
-  });
+  })
+}
 
 const app = express();
 
 app.get('/lorem', function (req, res) {
-  res.send(output);
+  res.send(generateHTML());
+})
+app.get('/lorem/:number', function (req, res) {
+  res.send(generateHTML(req.params.number));
 })
 
 app.listen(3000, function() {
