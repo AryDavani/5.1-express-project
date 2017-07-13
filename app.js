09ea6d35db29addb46162a3eac366add73f534b0
Ary Davani <arydavani@gmail.com>
2017-07-13 11:18:08 -0400

    working port

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get('/lorem/:number', function (req, res) {
   res.send(generateHTML(req.params.number));
 })
 
-app.listen(8080, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log('express app waiting for a response...');
 })
